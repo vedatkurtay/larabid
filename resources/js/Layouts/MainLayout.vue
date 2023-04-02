f2398a00d6b3819015ec92979c625e52fa44f0b8
Vedat Kurtay <vedatkurtay@gmail.com>
2023-04-03 01:31:00 +0300

    Added flash messages for the created data

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -1,15 +1,19 @@
 <template>
-   <Link href="/">Index</Link>&nbsp;
+    <div v-if="flashSuccess" class="alert">
+        {{ flashSuccess }}
+    </div>
+    <Link href="/">Index</Link>&nbsp;
     <Link href="/show">Show Page</Link>
-    <div>Time count is: {{ timer }}</div>
     <slot></slot>
 </template>
 
 <script setup>
-import { Link } from '@inertiajs/vue3'
-import { ref } from 'vue'
-const timer = ref(0)
-setInterval(() => {
-    timer.value++
-}, 1000)
+import {Link, usePage} from '@inertiajs/vue3'
+import {computed} from "vue";
+
+const page = usePage()
+const flashSuccess = computed(() =>
+    usePage().props.flash.success,
+)
 </script>
+
