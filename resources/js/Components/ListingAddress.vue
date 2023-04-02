83506866b2c70110ae875f209e7938da9bfe4b6e
Vedat Kurtay <vedatkurtay@gmail.com>
2023-04-02 23:02:29 +0300

    Added vue component and edited index,show page

---
+++ resources/js/Components/ListingAddress.vue
@@ -0,0 +1,11 @@
+<template>
+    <span>
+        {{ listing.street}} {{ listing.street_nr}} ,{{ listing.city }} for {{ listing.price }}€
+    </span>
+</template>
+
+<script setup>
+defineProps({
+    listing: Object,
+})
+</script>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\IndexController;
+use App\Http\Controllers\ListingController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,3 +16,5 @@ use Illuminate\Support\Facades\Route;
 */
 Route::get('/', [IndexController::class, 'index']);
 Route::get('/show', [IndexController::class, 'show']);
+
+Route::resource('listing', ListingController::class)->only('index', 'show');
